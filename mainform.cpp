5b9367b88d235bb77902be69732b97208589099b
kaje44 <kaje44@gmail.com>
2013-03-15 14:07:51 +0100

    uprava zada

--- mainform.cpp
+++ mainform.cpp
@@ -3,7 +3,7 @@
 **                                                                  **
 **  Vytvořen: St 27.úno.2013 08:16:49                               **
 **                                                                  **
-**  Posledni upravy: Pá 08.bře.2013 11:59:51                        **
+**  Posledni upravy: Pá 15.bře.2013 13:03:32                        **
 **********************************************************************/
 
 #include <QtGui>
@@ -20,7 +20,7 @@ SqlEngine *sqle;
 MainForm::MainForm() : KjMainWindow() {
 	setupUi(this);
 	if ( schliesser() ) {
-		createInfo("$build v1.0.4$", ":img/story-editor.png");
+		createInfo("$build v1.0.5$", ":img/story-editor.png");
 		m_rootDir = QCoreApplication::applicationDirPath()+"/";
 		syslog.open(m_rootDir + "infowork.log");		
 //		sqle = new SqlEngine("InfODBC","servercon","QODBC");
